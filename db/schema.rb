cd9f849f37d23ca18b22140287600981db497192
takurou Matsuo <m.taku7@gmail.com>
2015-08-23 05:06:42 +0900

    6.19

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150821004742) do
+ActiveRecord::Schema.define(version: 20150822195921) do
 
   create_table "users", force: true do |t|
     t.string   "name"
