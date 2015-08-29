9c5f69d582dc650d104a5dce6498ef0e4f87d5a2
takurou Matsuo <m.taku7@gmail.com>
2015-08-29 22:17:06 +0900

    list7.22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,20 @@ class UsersController < ApplicationController
 
 	def new
         @user = User.new
-	end
+    end
+
+    def create
+        @user = User.new(params[:user])
+        if @user.save
+
+        else
+            render 'new'
+        end
+    end
+
+    private
+        def user_params
+            params.require(:user).permit(:name,:email,:password,
+                :password_cofirmation)
+        end
 end
