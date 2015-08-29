b86102616dca0c04b68c7d71be7cae03a3bb77ea
takurou Matsuo <m.taku7@gmail.com>
2015-08-29 23:08:58 +0900

    7.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,14 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    def create
-        @user = User.new(params[:user])
-        if @user.save
-
-        else
-            render 'new'
-        end
+ def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
     end
+  end
 
     private
         def user_params
