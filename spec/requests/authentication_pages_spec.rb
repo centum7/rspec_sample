492406150e995aa47f2b11dcecd23df1855a0855
takurou Matsuo <m.taku7@gmail.com>
2015-08-31 01:55:22 +0900

    list8.7

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,18 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_content('Sign in') }
-    it { should have_title('Sign in') }
+    describe "with invalid information" do
+        let(:user) { FactoryGirl.create(:user) }
+        before do
+                fill_in "Email",with:user.email.upcase
+                fill_in "Password",with:user.Password
+                click_button "Sign in"
+            end
+
+        it { should have_title(user.name) }
+        it { should have_content('Sign in') }
+        it { should have_title('Sign in') }
+        it { should_not have_link('Sign in',)}
+    end
   end
 end
