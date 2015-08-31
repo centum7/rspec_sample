1f6a8a415c06dd1a7253c319dc2a50a7eedaf93b
takurou Matsuo <m.taku7@gmail.com>
2015-09-01 00:55:56 +0900

    list8.12

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
         else
-            flash.now[:error] = 'Inbalid email/password combination'
+            flash.now[:error] = 'Invalid email/password combination'
             render 'new'
+        end
     end
 
     def destroy
